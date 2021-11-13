4dcef4e1947af5e44c5461ee9ae8d5ba46269946
Rafi Priatna K <rafipriatnak@gmail.com>
2021-11-14 00:15:50 +0700

    Halaman muka destinasi

--- frontend/pages/destinasi/index.vue
+++ frontend/pages/destinasi/index.vue
@@ -2,7 +2,6 @@
   <div class="lg:px-0 px-4">
     <Navbar />
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-10">
-      <h1 class="text-4xl font-bold text-center my-20">Destinasi Wisata</h1>
       <Map class="h-1/2" />
       <Footer />
     </div>
