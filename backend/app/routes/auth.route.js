f55e4fb90daed6f914fcde8c8bf2efd2c5a7eb79
Rafi Priatna K <rafipriatnak@gmail.com>
2021-11-14 00:13:42 +0700

    Cek token

--- backend/app/routes/auth.route.js
+++ backend/app/routes/auth.route.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
 const controller = require('../controller/auth.controller');
 const router = express.Router()
 
 router.post('/login', controller.login)
 router.post('/register', controller.register)
+router.get(
+    "/",
+    passport.authenticate("adminAuth", {
+        session: false,
+    }),
+    (req, res) => {
+        return res.status(200).json({
+            message: "Data user yang login.",
+            user: req.user
+        });
+    }
+);
 
 
 module.exports = router
