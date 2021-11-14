086744bca71c45382e369b8f7c61099a0a5da1b1
Rafi Priatna K <rafipriatnak@gmail.com>
2021-11-14 14:11:07 +0700

    Ganti halaman wisata

--- frontend/components/Admin/Sidebar.vue
+++ frontend/components/Admin/Sidebar.vue
@@ -1,38 +1,29 @@
 <template>
   <aside class="relative bg-blue-600 h-screen w-64 hidden sm:block shadow-xl">
     <div class="p-6">
-      <a
-        href="index.html"
+      <nuxtLink
+        to="/admin"
         class="text-white text-3xl font-semibold uppercase hover:text-gray-300"
-        >Admin</a
+        >Admin</nuxtLink
       >
-      <button
-        class="
-          w-full
-          bg-white
-          text-blue-600
-          font-semibold
-          py-2
-          mt-5
-          rounded-br-lg rounded-bl-lg rounded-tr-lg
-          shadow-lg
-          hover:shadow-xl hover:bg-gray-300
-          flex
-          items-center
-          justify-center
-        "
-      >
-        <i class="fas fa-plus mr-3"></i> New Report
-      </button>
     </div>
     <nav class="text-white text-base font-semibold pt-3">
-      <a
-        href="index.html"
-        class="flex items-center bg-blue-800 text-white py-4 pl-6"
+      <nuxtLink
+        to="/admin"
+        class="flex items-center text-white py-4 pl-6 hover:bg-blue-800"
+        exact-active-class="bg-blue-900"
       >
         <i class="fas fa-tachometer-alt mr-3"></i>
         Dashboard
-      </a>
+      </nuxtLink>
+      <nuxtLink
+        to="/admin/wisata"
+        class="flex items-center text-white py-4 pl-6 hover:bg-blue-800"
+        exact-active-class="bg-blue-900"
+      >
+        <i class="fas fa-map-marked-alt mr-3"></i>
+        Destinasi Wisata
+      </nuxtLink>
     </nav>
     <a
       href="#"
