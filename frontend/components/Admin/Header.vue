4d2186780b55ba139491b85ca33f82ab6a6ce92c
Rafi Priatna K <rafipriatnak@gmail.com>
2021-11-14 11:47:37 +0700

    Interaktif admin panel

--- frontend/components/Admin/Header.vue
+++ frontend/components/Admin/Header.vue
@@ -1,37 +1,123 @@
 <template>
-  <!-- Desktop Header -->
-  <header
-    class="
-      w-full
-      items-center
-      bg-gray-100
-      py-2
-      px-6
-      hidden
-      sm:flex
-      border border-b-2
-    "
-  >
-    <div class="w-1/2"></div>
-    <div class="relative w-1/2 flex justify-end">
-      <button class="relative z-10 h-12">{{ this.name }}</button>
-      <button class="h-full w-full fixed inset-0 cursor-default"></button>
-      <div class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-        <a href="#" class="block px-4 py-2 account-link hover:text-white"
-          >Account</a
+  <div>
+    <!-- Desktop Header -->
+    <header
+      class="
+        w-full
+        items-center
+        bg-gray-100
+        py-2
+        px-6
+        hidden
+        sm:flex
+        border border-b-2
+      "
+    >
+      <div class="w-1/2"></div>
+      <div class="relative w-1/2 flex justify-end">
+        <button
+          @click="isRightNavClicked = !isRightNavClicked"
+          class="
+            relative
+            z-10
+            h-12
+            px-2
+            rounded
+            hover:bg-blue-600 hover:text-white
+          "
         >
-        <a href="#" class="block px-4 py-2 account-link hover:text-white"
-          >Support</a
+          {{ this.name }}
+        </button>
+        <div
+          v-if="isRightNavClicked"
+          class="absolute w-32 rounded-lg shadow-lg py-2 mt-16"
         >
+          <a
+            @click="logout"
+            href="#"
+            class="block px-4 py-2 hover:text-white hover:bg-blue-600"
+            >Keluar</a
+          >
+        </div>
+      </div>
+    </header>
+
+    <!-- Mobile Header & Nav -->
+    <header class="w-full bg-blue-800 py-5 px-6 sm:hidden">
+      <div class="flex items-center justify-between">
+        <a
+          href="index.html"
+          class="
+            text-white text-3xl
+            font-semibold
+            uppercase
+            hover:text-gray-300
+          "
+          >Admin</a
+        >
+        <button
+          @click="mobileNavClicked"
+          class="text-white text-3xl focus:outline-none"
+          v-html="navMobileIcon"
+        ></button>
+      </div>
+
+      <!-- Dropdown Nav -->
+      <nav :class="isMobile ? 'flex' : 'hidden'" class="flex flex-col pt-4">
+        <a
+          href="index.html"
+          class="
+            flex
+            items-center
+            text-white
+            opacity-75
+            hover:opacity-100
+            py-2
+            pl-4
+            nav-item
+          "
+        >
+          <i class="fas fa-tachometer-alt mr-3"></i>
+          Dashboard
+        </a>
         <a
           @click="logout"
           href="#"
-          class="block px-4 py-2 account-link hover:text-white"
-          >Sign Out</a
+          class="
+            flex
+            items-center
+            text-white
+            opacity-75
+            hover:opacity-100
+            py-2
+            pl-4
+            nav-item
+          "
         >
-      </div>
-    </div>
-  </header>
+          <i class="fas fa-sign-out-alt mr-3"></i>
+          Keluar
+        </a>
+        <button
+          class="
+            w-full
+            bg-white
+            cta-btn
+            font-semibold
+            py-2
+            mt-3
+            rounded-lg
+            shadow-lg
+            hover:shadow-xl hover:bg-gray-300
+            flex
+            items-center
+            justify-center
+          "
+        >
+          <i class="fas fa-arrow-circle-up mr-3"></i> Upgrade to Pro!
+        </button>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <script>
@@ -43,11 +129,21 @@ export default {
   data() {
     return {
       name: null,
+      isMobile: false,
+      isRightNavClicked: false,
+      navMobileIcon: "<i class='fas fa-bars'></i>",
     };
   },
   methods: {
     getData() {
       this.name = JSON.parse(JSON.stringify(this.loggedInUser)).name;
+    },
+    mobileNavClicked() {
+      this.isMobile = !this.isMobile;
+      this.navMobileIcon =
+        this.navMobileIcon == "<i class='fas fa-bars'></i>"
+          ? "<i class='fas fa-times'></i>"
+          : "<i class='fas fa-bars'></i>";
     },
     async logout(e) {
       e.preventDefault();
--- frontend/components/Admin/Sidebar.vue
+++ frontend/components/Admin/Sidebar.vue
@@ -1,15 +1,16 @@
 <template>
-  <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
+  <aside class="relative bg-blue-600 h-screen w-64 hidden sm:block shadow-xl">
     <div class="p-6">
       <a
         href="index.html"
         class="text-white text-3xl font-semibold uppercase hover:text-gray-300"
-        >Admin</a>
+        >Admin</a
+      >
       <button
         class="
           w-full
           bg-white
-          cta-btn
+          text-blue-600
           font-semibold
           py-2
           mt-5
@@ -27,90 +28,10 @@
     <nav class="text-white text-base font-semibold pt-3">
       <a
         href="index.html"
-        class="flex items-center active-nav-link text-white py-4 pl-6 nav-item"
+        class="flex items-center bg-blue-800 text-white py-4 pl-6"
       >
         <i class="fas fa-tachometer-alt mr-3"></i>
         Dashboard
-      </a>
-      <a
-        href="blank.html"
-        class="
-          flex
-          items-center
-          text-white
-          opacity-75
-          hover:opacity-100
-          py-4
-          pl-6
-          nav-item
-        "
-      >
-        <i class="fas fa-sticky-note mr-3"></i>
-        Blank Page
-      </a>
-      <a
-        href="tables.html"
-        class="
-          flex
-          items-center
-          text-white
-          opacity-75
-          hover:opacity-100
-          py-4
-          pl-6
-          nav-item
-        "
-      >
-        <i class="fas fa-table mr-3"></i>
-        Tables
-      </a>
-      <a
-        href="forms.html"
-        class="
-          flex
-          items-center
-          text-white
-          opacity-75
-          hover:opacity-100
-          py-4
-          pl-6
-          nav-item
-        "
-      >
-        <i class="fas fa-align-left mr-3"></i>
-        Forms
-      </a>
-      <a
-        href="tabs.html"
-        class="
-          flex
-          items-center
-          text-white
-          opacity-75
-          hover:opacity-100
-          py-4
-          pl-6
-          nav-item
-        "
-      >
-        <i class="fas fa-tablet-alt mr-3"></i>
-        Tabbed Content
-      </a>
-      <a
-        href="calendar.html"
-        class="
-          flex
-          items-center
-          text-white
-          opacity-75
-          hover:opacity-100
-          py-4
-          pl-6
-          nav-item
-        "
-      >
-        <i class="fas fa-calendar mr-3"></i>
-        Calendar
       </a>
     </nav>
     <a
@@ -118,9 +39,8 @@
       class="
         absolute
         w-full
-        upgrade-btn
         bottom-0
-        active-nav-link
+        bg-blue-800
         text-white
         flex
         items-center
@@ -135,11 +55,8 @@
 </template>
 
 <script>
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
-export default {
-
-}
+export default {};
 </script>
-
