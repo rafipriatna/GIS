90249508482b63465e8ad9f5ae1ed5bb618cf021
Rafi Priatna K <rafipriatnak@gmail.com>
2022-01-03 09:21:08 +0700

    Penambahan map pada halaman destinasi

--- frontend/pages/destinasi/index.vue
+++ frontend/pages/destinasi/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <!-- <Map class="h-1/2" /> -->
+    <Map class="h-1/2" :lokasiWisata="lokasiWisata" />
   </div>
 </template>
 
 <script>
 export default {
   layout: "visitor",
+  data() {
+    return {
+      lokasiWisata: [],
+    };
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("getWisataPengunjung").then((res) => {
+        this.lokasiWisata = res
+      });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
 };
 </script>
